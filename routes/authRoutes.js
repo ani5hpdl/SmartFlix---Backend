cf99d2943bf258928583a015d438cc44d288f46e
ani5hpdl <178013051+ani5hpdl@users.noreply.github.com>
2026-03-01 15:05:38 +0545

    feat(auth-routes) : revise auth endpoints and route wiring

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,11 @@
-const { registerUser, login, verify } = require('../controller/authController');
+const { registerUser, login, verify, getMe } = require('../controller/authController');
+const authGuard = require('../helpers/authguard');
 
 const express = require('express').Router();
 
 express.post('/register',registerUser);
 express.post('/login',login);
 express.get('/verify',verify);
+express.get('/getMe',authGuard,getMe);
 
 module.exports = express;
